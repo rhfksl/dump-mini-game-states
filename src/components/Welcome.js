c27508acdb5991ac88f841da5f02df35be1ea9b8
rhfksl <wkaqkdrn@gmail.com>
2020-04-10 19:12:22 +0900

    :construction: App 컴포넌트 path 분기 완료
     - 초기 홈페이지 화면 path분기 완료
     - 회원가입 버튼 클릭시 Signup 페이지로 이동
     - Guest로 입장 버튼 클릭시 Guest로 main페이지 이동
     - 로그인 버튼 클릭 시 nickname과 isLogined state 변경 후 main페이지로 이동

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import Main from './Main';
+import { Link, withRouter } from 'react-router-dom';
 
-// signup 분기점 필요
-function Welcome() {
+function Welcome(props) {
   return (
     <div>
       this is Welcome component
-      <Main />
+      {/* 아래 Link Component를 이용하여 회원가입 창으로 이동하세요 */}
+      <Link to="/signup">회원 가입을 하러 가즈아!!</Link>
+      {/* 로그인 성공 시 history.push를 이용하여 main 창으로 이동하세요 */}
+      <button
+        type="button"
+        onClick={() => {
+          // store의 isLogined 상태 변경
+          props.login();
+          // 아래 함수를 통해 store의 닉네임 변경 후 입장.
+          props.changeNickname('ㅆㅂ..');
+          // main페이지로 이동
+          props.history.push('main');
+        }}
+      >
+        로그인 성공시 setState변경
+      </button>
+      {/* guest로 입장 시 아래 링크를 이용하여 main 창으로 이동하세요 */}
+      <Link to="/main">guest 로그인 하러 가즈아!!</Link>
     </div>
   );
 }
 
-export default Welcome;
+export default withRouter(Welcome);
