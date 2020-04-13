d3a4e5c971b0debb41f482a6b39044ff14568928
rhfksl <wkaqkdrn@gmail.com>
2020-04-14 03:43:35 +0900

    :zap: Menu 항목들 Link 설정
     - 각각 항목마다 해당 path로 이동하게끔 Link 설정

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,62 +1,24 @@
 import React from 'react';
 import './Main.css';
+import { withRouter, Link } from 'react-router-dom';
 
-function Menu(props) {
+function Menu() {
   return (
     <div id="sidebar-wrapper">
       <ul className="sidebar-nav">
-        <li className="d-xl-flex sidebar-brand" id="BTImg"></li>
+        <li className="d-xl-flex sidebar-brand" id="BTImg" />
         <li>
-          <div id="menuGame" type="button" /*onclick={() => props}*/>
-            Game
-          </div>
+          <Link to="/Game1">Game</Link>
         </li>
         <li>
-          <div id="menuScore" type="button">
-            Score board
-          </div>
+          <Link to="/LeaderBoard">Leader Board</Link>
         </li>
         <li>
-          <div id="menuNotice" type="button">
-            Notice board
-          </div>
+          <Link to="/NoticeBoard">Notice Board</Link>
         </li>
       </ul>
     </div>
-    // <div id="top">
-    //   This is Menu component
-    //   <div>
-    //     This is GameTap
-    //     <input
-    //       type="button"
-    //       value="Click"
-    //       onClick={() => {
-    //         props.changeDisplayMode('Game');
-    //       }}
-    //     />
-    //   </div>
-    //   <div>
-    //     This is NoticeBoard
-    //     <input
-    //       type="button"
-    //       value="Click"
-    //       onClick={() => {
-    //         props.changeDisplayMode('NoticeBoard');
-    //       }}
-    //     />
-    //   </div>
-    //   <div>
-    //     This is LeaderBoard
-    //     <input
-    //       type="button"
-    //       value="Click"
-    //       onClick={() => {
-    //         props.changeDisplayMode('LeaderBoard');
-    //       }}
-    //     />
-    //   </div>
-    // </div>
   );
 }
 
-export default Menu;
+export default withRouter(Menu);
