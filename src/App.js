fd460003c559a53606843a0dc3d1cb63ffc3afad
rhfksl <wkaqkdrn@gmail.com>
2020-04-13 21:23:21 +0900

    reset path in App file

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { withRouter, Route, Switch } from 'react-router-dom';
 import Welcome from './containers/Welcome';
 import Signup from './components/Signup';
 import Main from './components/Main';
+import Game1 from './components/games/Game1';
+import NoticeBoard from './containers/NoticeBoard';
+import LeaderBoard from './containers/LeaderBoard';
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Route exact path="/" render={() => <Welcome />} />
         <Route exact path="/signup" render={() => <Signup />} />
         <Route exact path="/main" render={() => <Main />} />
+        <Route exact path="/Game1" render={() => <Game1 />} />
+        <Route exact path="/LeaderBoard" render={() => <LeaderBoard />} />
+        <Route exact path="/NoticeBoard" render={() => <NoticeBoard />} />
       </Switch>
     </div>
   );
